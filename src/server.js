a31087b39a1a0388ff4942680e1a2493672dcbff
duythong114 <tranthong.facebook@gmail.com>
2024-04-06 18:29:07 +0700

    change variable of env

--- src/server.js
+++ src/server.js
@@ -18,7 +18,7 @@ connectDB();
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', process.env.REACT_URL);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
